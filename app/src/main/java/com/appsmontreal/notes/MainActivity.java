7f9b98979f49605faeb0907d975be654dba3d24b
Marlon <escobar.marlon@gmail.com>
2019-10-11 16:48:09 -0400

    Menu created

--- app/src/main/java/com/appsmontreal/notes/MainActivity.java
+++ app/src/main/java/com/appsmontreal/notes/MainActivity.java
@@ -1,8 +1,12 @@
 package com.appsmontreal.notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //To inflate menu
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch(item.getItemId()){
+            case R.id.addNote:
+                break;
+
+            case R.id.exit:
+                finish();
+                break;
+
+            default:
+                break;
+        }
+
+        return true;
     }
 }
