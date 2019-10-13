9f0d25f53722933aab3f4bf17e9620a384d3f494
Marlon <escobar.marlon@gmail.com>
2019-10-12 21:30:25 -0400

    onActivityResult method created

--- app/src/main/java/com/appsmontreal/notes/CreateNoteActivity.java
+++ app/src/main/java/com/appsmontreal/notes/CreateNoteActivity.java
@@ -2,25 +2,44 @@ package com.appsmontreal.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.appsmontreal.notes.Serializer.ObjectSerializer;
 
 public class CreateNoteActivity extends AppCompatActivity {
-    private SharedPreferences sharedPreferences;
+
     private EditText noteEditText;
-    private ObjectSerializer objectSerializer;
+    private Intent returnIntent;
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_note);
-        sharedPreferences = getSharedPreferences("NOTES", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
         noteEditText = findViewById(R.id.noteEditText);
+        saveButton = findViewById(R.id.saveButton);
+        saveNote();
 
     }
+
+    private void saveNote() {
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                returnIntent = new Intent();
+                returnIntent.putExtra("NOTE",noteEditText.getText().toString());
+                setResult(RESULT_OK,returnIntent);
+                finish();
+            }
+        });
+    }
+
+
 }
--- app/src/main/java/com/appsmontreal/notes/MainActivity.java
+++ app/src/main/java/com/appsmontreal/notes/MainActivity.java
@@ -1,22 +1,35 @@
 package com.appsmontreal.notes;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.appsmontreal.notes.Serializer.ObjectSerializer;
+
 public class MainActivity extends AppCompatActivity {
     private Intent intent;
+    private SharedPreferences sharedPreferences;
+    private ObjectSerializer objectSerializer;
+    private SharedPreferences.Editor editor;
+    private String note;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         intent = new Intent(this,CreateNoteActivity.class);
+        sharedPreferences = getSharedPreferences("DATA", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
     }
 
     //To inflate menu
@@ -33,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         super.onOptionsItemSelected(item);
         switch(item.getItemId()){
             case R.id.addNote:
-                startActivity(intent);
+                startActivityForResult(intent,911);
                 break;
 
             case R.id.exit:
@@ -45,5 +58,19 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return true;
+    }
+
+    //Handling the note that's coming from CreateNoteActivity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 911){
+            if (resultCode == RESULT_OK) {
+                note = data.getStringExtra("NOTE");
+                Log.i("----------------->", note);
+            }
+        }
+
     }
 }
